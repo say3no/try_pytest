ee40083d240a297b35c2c0d7f4d2fad93ed7cb22
say3no <say3no@gmail.com>
2019-01-03 14:21:44 +0900

    hoge

--- tests/hoge_test.py
+++ tests/hoge_test.py
@@ -1,4 +1,5 @@
 # vim: set fileencoding=utf-8 :
+import pytest
 from src.hoge import Hoge
 
 # @pytest.fixture(scope='session', autouse=True)
@@ -10,6 +11,20 @@ from src.hoge import Hoge
 
 def test_constractor():
     a, b = (1, 2)
-    h = Hoge(a, b)
-    assert h.a == a
-    assert h.b == str(b)
+    reg = Hoge(a, b)
+    assert hasattr(reg, 'a')
+    assert hasattr(reg, 'b')
+    assert reg.a == a
+    assert reg.b == str(b)
+    assert str(b) in reg.b
+
+    # errorはこんなかんじ
+    with pytest.raises(TypeError) as e:
+        irr = Hoge()
+
+
+# pytest.raises(SomeError)のかわりに、
+# こういったデコレータでも異常系のテストを書ける。便利
+@pytest.mark.xfail(raises=TypeError)
+def test_pytest_error():
+    irr = Hoge()
